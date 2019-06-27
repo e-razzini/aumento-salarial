9911b8eeb697c30d6260839e3a7cc07fa670861f
Aluno <Aluno@SN-279228>
2019-06-26 21:37:51 -0300

    Iniciado

---
+++ src/cargo/AumentoSalarial.java
@@ -0,0 +1,111 @@
+package cargo;
+
+import modelo.Funcionario;
+
+public class AumentoSalarial {
+
+    private Funcionario func;
+
+    public AumentoSalarial(Funcionario func) {
+        this.func = func;
+    }
+
+    public double calcularAumento() {
+        
+        double novoSalario = 0;
+        int codCargo = this.func.getCodCargo();
+        
+        if (codCargo == 101) {
+            novoSalario = this.calcGerente();
+        } else if (codCargo == 102) {
+            novoSalario = this.calcEngenheiro();
+        } else if (codCargo == 103) {
+            novoSalario = this.calcTecnico();
+        } else {
+            novoSalario = this.calcGeral();
+        }
+        
+        return novoSalario;
+    }
+
+    private double calcGerente() {
+
+        int tempo = this.func.getTempServico();
+        double percentual = 0;
+
+        if (tempo >= 1 & tempo < 2) {
+            percentual = 10;
+        } else if (tempo <= 3) {
+            percentual = 20;
+        } else {
+            percentual = 25;
+        }
+
+        double salario = this.func.getSalario();
+
+        double novoSalario = (salario * percentual) / 100;
+        novoSalario = salario + novoSalario;
+
+        return novoSalario;
+    }
+
+    private double calcEngenheiro() {
+
+        int tempo = this.func.getTempServico();
+        double percentual = 0;
+
+        if (tempo >= 1 & tempo < 2) {
+            percentual = 15;
+        } else if (tempo <= 3) {
+            percentual = 30;
+        } else {
+            percentual = 35;
+        }
+
+        double salario = this.func.getSalario();
+
+        double novoSalario = (salario * percentual) / 100;
+        novoSalario = salario + novoSalario;
+
+        return novoSalario;
+
+    }
+
+    private double calcTecnico() {
+        int tempo = this.func.getTempServico();
+        double percentual = 0;
+
+        if (tempo >= 1 & tempo < 2) {
+            percentual = 5;
+        } else if (tempo <= 3) {
+            percentual = 10;
+        } else {
+            percentual = 15;
+        }
+
+        double salario = this.func.getSalario();
+
+        double novoSalario = (salario * percentual) / 100;
+        novoSalario = salario + novoSalario;
+
+        return novoSalario;
+    }
+
+    private double calcGeral() {
+
+        int tempo = this.func.getTempServico();
+        double percentual = 0;
+
+        if (tempo >= 3) {
+            percentual = 35;
+        }
+
+        double salario = this.func.getSalario();
+
+        double novoSalario = (salario * percentual) / 100;
+        novoSalario = salario + novoSalario;
+
+        return novoSalario;
+    }
+
+}
---
+++ src/modelo/Funcionario.java
@@ -0,0 +1,42 @@
+package modelo;
+
+public class Funcionario {
+ 
+    private String nome;
+    private double salario;
+    private int codCargo;
+    private int tempServico;
+    
+    public String getNome() {
+        return this.nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getCodCargo() {
+        return codCargo;
+    }
+
+    public void setCodCargo(int codCargo) {
+        this.codCargo = codCargo;
+    }
+
+    public int getTempServico() {
+        return tempServico;
+    }
+
+    public void setTempServico(int tempServico) {
+        this.tempServico = tempServico;
+    }
+    
+}
